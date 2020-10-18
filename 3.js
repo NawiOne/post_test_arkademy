54e9485303799173fbc974d2c01906c341ae4ffb
shadwoe <rudinnawi@gmail.com>
2020-10-18 12:03:51 +0700

    commit

--- 3.js
+++ 3.js
@@ -1,1 +1,1 @@
-const query = "SELECT name, rides.distance AS distance_traveled, (rides.distance*rides.fare) AS total_fares FROM users JOIN rides ON users.id = rides.user_id ORDER BY total_fares DESC LIMIT 10";
+const query = "SELECT name, rides.distance AS distance_traveled, (rides.distance*rides.fare) AS total_fares FROM users JOIN rides ON users.id = rides.user_id ORDER BY distance_traveled DESC LIMIT 10";
