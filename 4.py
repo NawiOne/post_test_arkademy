2e2a4283de4b924a27cb9518bbebf24e0f3034dd
shadwoe <rudinnawi@gmail.com>
2020-10-18 15:24:20 +0700

    commit

--- 4.py
+++ 4.py
@@ -7,8 +7,7 @@ def segitiga(num) :
             for j in range(1,i) : 
                 print (" ", end="") 
                   
-            for j in range(1,(num * 2 - (2 * i - 1))  
-                                           +1) : 
+            for j in range(1,(num * 2 - (2 * i - 1)) +1) : 
                 if (i == 1 or j == 1 or 
                    j == (num * 2 - (2 * i - 1))) : 
                     print ("0", end="")  
