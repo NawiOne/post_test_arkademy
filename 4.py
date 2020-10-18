5a46b0d07169f4894687d357296c14166a8bea42
shadwoe <rudinnawi@gmail.com>
2020-10-18 11:25:35 +0700

    commit

--- 4.py
+++ 4.py
@@ -1,41 +1,23 @@
-# // const print_triangle = (num) => {
-# //     for(let i = 1; i <= num; i++){
-# //         for(let j =1; j < i; j++){
-# //             console.log(' ')
-# //         }
-# //         for(let j = 1; j <= (num * 2 - (2* i - 1)); j++){
-# //             if(i === 1 || j === 1 || j === (num * 2 - (2 * i -1))){
-# //                 console.log('#')
-# //             } else console.log(' ')
-# //         }
-# //         console.log('\n')
-# //     }
-
-
-# // };
-
-# // print_triangle(5);
-
-def printPattern(n) : 
-      
-    for i in range(1,n+1) : 
-          
-        # Print spaces 
-        for j in range(1,i) : 
-            print (" ", end="") 
+def segitiga(num) :
+    if (num % 2) == 1:
+        print('angka harus genap boy')
+    else:
+        for i in range(1,num+1) : 
               
-        # Print hollow inverted pyramid 
-        for j in range(1,(n * 2 - (2 * i - 1))  
-                                       +1) : 
-            if (i == 1 or j == 1 or 
-               j == (n * 2 - (2 * i - 1))) : 
-                print ("#", end="")  
-            else : 
-                print(" ", end="")  
+            for j in range(1,i) : 
+                print (" ", end="") 
                   
-        # next line 
-        print (""), 
-  
-# Driver code 
-n = 6
-printPattern(n) 
+            for j in range(1,(num * 2 - (2 * i - 1))  
+                                           +1) : 
+                if (i == 1 or j == 1 or 
+                   j == (num * 2 - (2 * i - 1))) : 
+                    print ("0", end="")  
+                else : 
+                    print(" ", end="")  
+                      
+            print (""), 
+        
+      
+segitiga(6) 
+
+# pyhton 3
